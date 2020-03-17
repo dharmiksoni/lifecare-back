cc69bfaaf67dc5d991d24a85a4dfdbe4488902b0
Anant Lalchandani <anant.lalchandani@gmail.com>
2020-03-17 23:18:27 +0530

    changed to mongodb atlas

--- routes/lifecare_route.js
+++ routes/lifecare_route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const LifeCtrl = require('../controller/lifecare-ctrl');
 
 const router = express.Router();
-
+console.log("inside routes")
 // router.post('/locationCreate', LifeCtrl.createLocation);
 router.get('/location', LifeCtrl.getLocation);
 
